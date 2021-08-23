0898c02486b28391ce91bf6d08361deb24e3fe90
llucasreis <pro.lucasreis@gmail.com>
2021-08-23 14:29:54 -0300

    feat: add query resolvers for reviews
    - EntityFetcher for Game
    - DgsQuery for reviews

--- review-dgs/src/main/java/com/llucasreis/reviewdgs/datafetchers/ReviewDataFetcher.java
+++ review-dgs/src/main/java/com/llucasreis/reviewdgs/datafetchers/ReviewDataFetcher.java
@@ -1,29 +1,49 @@
 package com.llucasreis.reviewdgs.datafetchers;
 
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 import com.llucasreis.reviewdgs.dataloaders.ReviewDataLoader;
-import com.llucasreis.reviewdgs.domain.entities.Game;
-import com.llucasreis.reviewdgs.dtos.ReviewDTO;
-import com.netflix.graphql.dgs.DgsComponent;
-import com.netflix.graphql.dgs.DgsData;
-import com.netflix.graphql.dgs.DgsDataFetchingEnvironment;
-import com.netflix.graphql.dgs.DgsEntityFetcher;
+import com.llucasreis.reviewdgs.domain.externals.Game;
+import com.llucasreis.reviewdgs.domain.entities.Review;
+import com.llucasreis.reviewdgs.services.ReviewService;
+import com.netflix.graphql.dgs.*;
 
 import org.dataloader.DataLoader;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @DgsComponent
 public class ReviewDataFetcher {
 
+  private final ReviewService reviewService;
+
+  @Autowired
+  public ReviewDataFetcher(ReviewService reviewService) {
+    this.reviewService = reviewService;
+  }
+
   @DgsEntityFetcher(name = "Game")
   public Game game(Map<String, Object> values) {
-    return Game.builder().id(Long.parseLong((String)values.get("id"))).build();
+    Long id = Long.parseLong((String) values.get("id"));
+
+    return Game.builder().id(id).build();
+  }
+
+  @DgsQuery
+  public List<Review> reviews() {
+    return this.reviewService.find();
+  }
+
+  @DgsData(parentType = "Review", field = "game")
+  public Game game(DgsDataFetchingEnvironment dfe) {
+    Review review = dfe.getSource();
+    return Game.builder().id(review.getGameId()).build();
   }
 
   @DgsData(parentType = "Game", field = "reviews")
-  public CompletableFuture<ReviewDTO> review(DgsDataFetchingEnvironment dfe) {
-    DataLoader<Long, ReviewDTO> reviewDataLoader = dfe.getDataLoader(ReviewDataLoader.class);
+  public CompletableFuture<Review> review(DgsDataFetchingEnvironment dfe) {
+    DataLoader<Long, Review> reviewDataLoader = dfe.getDataLoader(ReviewDataLoader.class);
 
     Game game = dfe.getSource();
 
