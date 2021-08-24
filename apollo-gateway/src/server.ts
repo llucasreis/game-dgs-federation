b1665eac4c2835460d645b720a2307bc6536deca
llucasreis <pro.lucasreis@gmail.com>
2021-08-24 14:59:08 -0300

    feat: add user dgs in apollo gateway

--- apollo-gateway/src/server.ts
+++ apollo-gateway/src/server.ts
@@ -4,7 +4,8 @@ import { ApolloGateway } from '@apollo/gateway';
 const gateway = new ApolloGateway({
   serviceList: [
     {name: "games", url: "http://localhost:8080/graphql"},
-    {name: "reviews", url: "http://localhost:8081/graphql"}
+    {name: "reviews", url: "http://localhost:8081/graphql"},
+    {name: "users", url: "http://localhost:8082/graphql"}
   ]
 });
 
--- user-dgs/src/main/java/com/llucasreis/userdgs/datafetchers/UserDataFetcher.java
+++ user-dgs/src/main/java/com/llucasreis/userdgs/datafetchers/UserDataFetcher.java
@@ -3,10 +3,12 @@ package com.llucasreis.userdgs.datafetchers;
 import com.llucasreis.userdgs.domain.entities.User;
 import com.llucasreis.userdgs.repositories.UserRepository;
 import com.netflix.graphql.dgs.DgsComponent;
+import com.netflix.graphql.dgs.DgsEntityFetcher;
 import com.netflix.graphql.dgs.DgsQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
+import java.util.Map;
 
 @DgsComponent
 public class UserDataFetcher {
@@ -21,5 +23,12 @@ public class UserDataFetcher {
     @DgsQuery
     public List<User> users() {
         return this.userRepository.findAll();
+    }
+
+    @DgsEntityFetcher(name = "User")
+    public User user(Map<String, Object> values) {
+        Long id = Long.parseLong((String) values.get("id"));
+
+        return this.userRepository.findById(id).orElse(null);
     }
 }
